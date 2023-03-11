eac546a42f58eee5abb232fc287004ca03ff2664
Nate Johnston <46363319+Millwr1ght@users.noreply.github.com>
2023-03-11 13:38:02 -0600

    Create utils.js
    https://i.kym-cdn.com/photos/images/original/001/264/842/220.png

---
+++ src/js/utils.js
@@ -0,0 +1,12 @@
+export function qs(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
+}
+// save data to local storage
+export function setLocalStorage(key, data) {
+  localStorage.setItem(key, JSON.stringify(data));
+}
