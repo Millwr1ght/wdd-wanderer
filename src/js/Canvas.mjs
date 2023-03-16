0569d1885590beebfc0f7082d5a601a6a04bd2cf
Nate J <46363319+Millwr1ght@users.noreply.github.com>
2023-03-16 13:38:10 -0500

    figuring it out

--- src/js/Canvas.mjs
+++ src/js/Canvas.mjs
@@ -5,11 +5,27 @@ export function draw(selector) {
 
     if(canvas.getContext) {
         const ctx = canvas.getContext("2d");
-
-        ctx.fillRect(25, 25, 100, 100);
-        ctx.clearRect(45, 45, 60, 60);
-        ctx.strokeRect(50, 50, 50, 50);
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+              ctx.fillStyle = `rgb(0, ${Math.floor(255 - 31.875 * i)}, ${Math.floor(
+                255 - 31.875 * j
+              )})`;
+              ctx.fillRect(j * 25, i * 25, 25, 25);
+            }
+        }
     } else {
         //drawing is unsupported, thus
     }
 };
+
+
+// drawing utilities
+function roundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + radius);
+    ctx.arcTo(x, y + height, x + radius, y + height, radius);
+    ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+    ctx.arcTo(x + width, y, x + width - radius, y, radius);
+    ctx.arcTo(x, y, x, y + radius, radius);
+    ctx.stroke();
+}
